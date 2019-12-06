0cad490b6009dcdb67776b90437df97d859fbc8e
linksboy <55168397+linksboy@users.noreply.github.com>
2019-12-06 01:56:37 -0600

    Add files via upload
    不同k值的benchmark

---
+++ examples/benchmark@k.py
@@ -0,0 +1,170 @@
+"""
+This module illustrates how to compute Precision at k and Recall at k metrics.
+"""
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from collections import defaultdict
+import time
+import datetime
+import random
+
+import numpy as np
+import six
+from tabulate import tabulate
+
+from surprise import Dataset
+from surprise.model_selection import cross_validate
+from surprise.model_selection import KFold
+from surprise import NormalPredictor
+from surprise import BaselineOnly
+from surprise import KNNBasic
+from surprise import KNNWithMeans
+from surprise import KNNBaseline
+from surprise import SVD
+from surprise import SVDpp
+from surprise import NMF
+from surprise import SlopeOne
+from surprise import CoClustering
+from surprise.model_selection import train_test_split
+
+classes = (SVD, SVDpp, NMF, SlopeOne, KNNBasic, KNNWithMeans, KNNBaseline,
+           CoClustering, BaselineOnly, NormalPredictor)
+
+Ks = range(5 , 30 , 5)
+
+# ugly dict to map algo names and datasets to their markdown links in the table
+stable = 'http://surprise.readthedocs.io/en/stable/'
+LINK = {'SVD': '[{}]({})'.format('SVD',
+                                 stable +
+                                 'matrix_factorization.html#surprise.prediction_algorithms.matrix_factorization.SVD'),
+        'SVDpp': '[{}]({})'.format('SVD++',
+                                   stable +
+                                   'matrix_factorization.html#surprise.prediction_algorithms.matrix_factorization.SVDpp'),
+        'NMF': '[{}]({})'.format('NMF',
+                                 stable +
+                                 'matrix_factorization.html#surprise.prediction_algorithms.matrix_factorization.NMF'),
+        'SlopeOne': '[{}]({})'.format('Slope One',
+                                      stable +
+                                      'slope_one.html#surprise.prediction_algorithms.slope_one.SlopeOne'),
+        'KNNBasic': '[{}]({})'.format('k-NN',
+                                      stable +
+                                      'knn_inspired.html#surprise.prediction_algorithms.knns.KNNBasic'),
+        'KNNWithMeans': '[{}]({})'.format('Centered k-NN',
+                                          stable +
+                                          'knn_inspired.html#surprise.prediction_algorithms.knns.KNNWithMeans'),
+        'KNNBaseline': '[{}]({})'.format('k-NN Baseline',
+                                         stable +
+                                         'knn_inspired.html#surprise.prediction_algorithms.knns.KNNBaseline'),
+        'CoClustering': '[{}]({})'.format('Co-Clustering',
+                                          stable +
+                                          'co_clustering.html#surprise.prediction_algorithms.co_clustering.CoClustering'),
+        'BaselineOnly': '[{}]({})'.format('Baseline',
+                                          stable +
+                                          'basic_algorithms.html#surprise.prediction_algorithms.baseline_only.BaselineOnly'),
+        'NormalPredictor': '[{}]({})'.format('Random',
+                                             stable +
+                                             'basic_algorithms.html#surprise.prediction_algorithms.random_pred.NormalPredictor'),
+        'ml-100k': '[{}]({})'.format('Movielens 100k',
+                                     'http://grouplens.org/datasets/movielens/100k'),
+        'ml-1m': '[{}]({})'.format('Movielens 1M',
+                                   'http://grouplens.org/datasets/movielens/1m'),
+        }
+
+
+def precision_recall_at_k(predictions, k=10, threshold=3.5):
+    '''Return precision and recall at k metrics for each user.'''
+
+    # First map the predictions to each user.
+    user_est_true = defaultdict(list)
+    for uid, _, true_r, est, _ in predictions:
+        user_est_true[uid].append((est, true_r))
+
+    precisions = dict()
+    recalls = dict()
+    for uid, user_ratings in user_est_true.items():
+
+        # Sort user ratings by estimated value
+        user_ratings.sort(key=lambda x: x[0], reverse=True)
+
+        # Number of relevant items
+        n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
+
+        # Number of recommended items in top k
+        n_rec_k = sum((est >= threshold) for (est, _) in user_ratings[:k])
+
+        # Number of relevant and recommended items in top k
+        n_rel_and_rec_k = sum(((true_r >= threshold) and (est >= threshold))
+                              for (est, true_r) in user_ratings[:k])
+
+        # Precision@K: Proportion of recommended items that are relevant
+        precisions[uid] = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else 1
+
+        # Recall@K: Proportion of relevant items that are recommended
+        recalls[uid] = n_rel_and_rec_k / n_rel if n_rel != 0 else 1
+
+    return precisions, recalls
+
+dataset = 'ml-100k'
+data = Dataset.load_builtin('ml-100k')
+kf = KFold(n_splits=5)
+trainset,testset = train_test_split(data,test_size=.75)
+'''
+for trainset, testset in kf.split(data):
+    algo.fit(trainset)
+    predictions = algo.test(testset)
+    precisions, recalls = precision_recall_at_k(predictions, k=5, threshold=4)
+
+    # Precision and recall can then be averaged over all users
+    prec = sum(p for p in precisions.values()) / len(precisions)
+    recall = sum(rec for rec in recalls.values()) / len(recalls)
+    f1 = 2 * prec * recall / (prec + recall)
+    print(prec)
+    print(recall)
+    print(f1)
+'''
+table = []
+for klass in classes:
+    start = time.time()
+    if klass == 'SVD':
+        algo = SVD()
+    elif klass == 'SVDpp':
+        algo = SVDpp()
+    elif klass == 'NMF':
+        algo = NMF()
+    elif klass == 'SlopeOne':
+        algo = SlopeOne()
+    elif klass == 'KNNBasic':
+        algo = KNNBasic()
+    elif klass == 'KNNWithMeans':
+        algo = KNNWithMeans()
+    elif klass == 'KNNBaseline':
+        algo = KNNBaseline()
+    elif klass == 'CoClustering':
+        algo = CoClustering()
+    elif klass == 'BaselineOnly':
+        algo = BaselineOnly()
+    else :
+        algo = NormalPredictor()
+    #cv_time = str(datetime.timedelta(seconds=int(time.time() - start)))
+    for k in Ks:
+        algo.fit(trainset)
+        predictions = algo.test(testset)
+        precisions, recalls = precision_recall_at_k(predictions, k=5, threshold=4)
+        prec = sum(p for p in precisions.values()) / len(precisions)
+        recall = sum(rec for rec in recalls.values()) / len(recalls)
+        f1 = 2 * prec * recall / (prec + recall)
+        link = LINK[klass.__name__]
+        link = '{}@{}'.format(klass.__name__,k)
+        new_line = [link, prec, recall, f1]
+        print(tabulate([new_line], tablefmt="pipe"))  # print current algo perf
+        table.append(new_line)
+
+    # Precision and recall can then be averaged over all users
+    
+header = [LINK[dataset],
+          'Precision',
+          'Recall',
+          'F1'
+          ]
+print(tabulate(table, header, tablefmt="pipe"))
